052a13ceb18ce209f83864026122ff64d5062cef
veskol1 <veskol1@gmail.com>
2023-01-05 11:25:25 +0200

    fix main screen ui and add Glide

--- app/src/main/java/com/example/newsapp/adapter/ArticlesAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapter/ArticlesAdapter.kt
@@ -3,6 +3,9 @@ package com.example.newsapp.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.newsapp.databinding.ListItemArticleBinding
 import com.example.newsapp.model.Article
 
@@ -19,8 +22,18 @@ class ArticlesAdapter(private val articles: ArrayList<Article>):
 
     override fun onBindViewHolder(holder: ArticlesViewHolder, position: Int) {
         with(holder) {
-            binding.apply {
-                newspaperName.text = articles[position].title
+            with(articles[position]) {
+                binding.apply {
+                    newspaperTitle.text = title
+                    newspaperName.text = name
+                    newspaperDesc.text = desc
+                    newspaperDate.text = date
+
+                    Glide.with(holder.itemView.context)
+                        .load(image)
+                        .transform(CenterCrop(), RoundedCorners(16))
+                        .into(articleImage)
+                }
             }
         }
     }
