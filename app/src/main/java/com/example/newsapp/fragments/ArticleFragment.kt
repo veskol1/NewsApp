2fdacfd26e251e9103cc9b0516828ecc6d6f3465
veskol1 <veskol1@gmail.com>
2023-01-05 09:16:28 +0200

    add view binding

--- app/src/main/java/com/example/newsapp/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/ArticleFragment.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentArticleBinding
+import com.example.newsapp.databinding.FragmentMainBinding
 
 class ArticleFragment : Fragment() {
+    private var _binding: FragmentArticleBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+        _binding = FragmentArticleBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/newsapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/MainFragment.kt
@@ -6,14 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
