42c626c5c6dfd2aa58267481e0af0cb238f583da
veskol1 <veskol1@gmail.com>
2023-01-05 09:58:26 +0200

    implement viewmodel uiState

--- app/src/main/java/com/example/newsapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/MainFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.newsapp.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.newsapp.databinding.FragmentMainBinding
+import com.example.newsapp.model.Article
+import com.example.newsapp.viewmodel.NewsViewModel
+import com.example.newsapp.viewmodel.Status
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+    private val newsViewModel: NewsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +32,34 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                newsViewModel.uiState.collect{ uiState ->
+                    when(uiState.status) {
+                        Status.LOADING -> {
+                            binding.progressBar.visibility = View.VISIBLE
+                            binding.errorView.visibility = View.GONE
+                            binding.recyclerView.visibility = View.GONE
+                        }
+                        Status.ERROR -> {
+                            binding.progressBar.visibility = View.GONE
+                            binding.errorView.visibility = View.VISIBLE
+                            binding.recyclerView.visibility = View.GONE
+                        }
+                        Status.DONE -> {
+                            binding.progressBar.visibility = View.GONE
+                            binding.errorView.visibility = View.GONE
+                            binding.recyclerView.visibility = View.VISIBLE
+                            uiState.articles?.let { configureRecyclerView(it) }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-
+    private fun configureRecyclerView(articles: ArrayList<Article>) {
+        //to do
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/newsapp/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/viewmodel/NewsViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.newsapp.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.newsapp.model.Article
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class NewsViewModel {
-
-
+class NewsViewModel: ViewModel() {
+    private val _uiState =
+        MutableStateFlow(UiState(status = Status.LOADING, articles = arrayListOf()))
+    var uiState: StateFlow<UiState> = _uiState.asStateFlow()
 }
 
 data class UiState(
-    val articles: ArrayList<Article>?,
-    val stateStatus: Status
+    val status: Status,
+    val articles: ArrayList<Article>?
 )
 
 enum class Status {
